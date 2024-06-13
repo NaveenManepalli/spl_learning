2a212a5e7996be4a19c1c397205b80eb311b19c5
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2024-06-13 21:53:10 +0530

    Create day1.sql

---
+++ day1.sql
@@ -0,0 +1,39 @@
+create database if not exists college;
+use college;
+
+create table student (
+
+  rollno int primary key,
+  name varchar(50)
+);
+
+select * from temp1;
+
+insert into student
+(rollno, name)
+values
+(101,"karan"),
+(102,"naveen"),
+(103,"ben");
+
+create table temp1(
+ id int unique
+);
+
+insert into temp1 value(101);
+insert into temp1 value(102);
+
+create table temp2(
+ id int not null
+);
+
+insert into temp2 value(101);
+insert into temp2 value(101);
+
+create table emp(
+  id int,
+  salary int default 25000);
+  
+insert into emp  (id)values (101);
+select * from emp;
+
