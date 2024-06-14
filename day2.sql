94cec57c55369c6b1b3623a5ffd5ca2bad6a7c19
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2024-06-14 18:15:42 +0530

    Update day2.sql

--- day2.sql
+++ day2.sql
@@ -1,1 +1,25 @@
+CREATE DATABASE education;
+USE education;
 
+CREATE TABLE student(
+	rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+INSERT INTO student
+(rollno, name, marks, grade, city)
+VALUES
+(101, "anil",78,"C","Pune"),
+(102, "bhumika",93,"A","Mumbai"),
+(103, "chetana",85,"B","Mumbai"),
+(104, "dhruv",96,"A","Delhi"),
+(105, "emanuel",12,"F","Delhi"),
+(106, "farah",82,"B","Delhi");
+
+select distinct city from student;
+
+select * from student
+where marks >80 and city= "Mumbai";
